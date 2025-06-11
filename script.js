98a311a76c2cf61a56b8aba050aed0b5e7d7899e
Renanxff2 <testegames37@gmail.com>
2025-06-10 21:08:30 -0300

    Mudanças no design do site e melhorias

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
+function mostrarSecao(id) {
+      document.getElementById("header").style.display = "none";
+      document.getElementById("sobre").style.display = "none";
+      document.getElementById("projetos").style.display = "none";
+      document.getElementById("redes-sociais").style.display = "none";
 
-  function mostrarSecao(id) {
-    // Esconde tudo
-    document.getElementById("header").style.display = "none";
-    document.getElementById("sobre").style.display = "none";
-    document.getElementById("projetos").style.display = "none";
-    document.getElementById("redes-sociais").style.display = "none";
+      document.getElementById(id).style.display = "block";
+    }
 
-    // Mostra só a seção clicada
-    document.getElementById(id).style.display = "block";
-  }
-
-  function voltar() {
-    // Volta ao header
-    document.getElementById("header").style.display = "block";
-    document.getElementById("sobre").style.display = "none";
-    document.getElementById("projetos").style.display = "none";
-    document.getElementById("redes-sociais").style.display = "none";
-  }
-
+    function voltar() {
+      document.getElementById("header").style.display = "block";
+      document.getElementById("sobre").style.display = "none";
+      document.getElementById("projetos").style.display = "none";
+      document.getElementById("redes-sociais").style.display = "none";
+    }
